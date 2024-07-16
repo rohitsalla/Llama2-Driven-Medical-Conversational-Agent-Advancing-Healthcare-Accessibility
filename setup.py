2215e6b9c5297e37a89e3e1b3cd60526efacfcab
rohitsalla <sallarohit1011@gmail.com>
2024-07-16 15:22:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Medical Chatbot',
     version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'rohit',
+    author_email= 'sallarohit1011@gmail.com',
     packages= find_packages(),
     install_requires = []
 
